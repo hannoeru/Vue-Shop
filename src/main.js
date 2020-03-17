760918eb5673b1483508364ab2f436a5525d8634
hanlee <me@hanlee.co>
2020-03-17 20:35:06 +0900

    Add axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+// axios
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+axios.defaults.withCredentials = true
+Vue.use(VueAxios, axios)
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
