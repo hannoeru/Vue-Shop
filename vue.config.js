9aa866f007a0edefbcdf85ccfefaf1c78e0d3b44
hanlee <me@hanlee.co>
2020-03-25 00:52:08 +0900

    change publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   transpileDependencies: ['quasar'],
-
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: '網路點數專賣'
+    }
+  },
+  publicPath: '/Vue-Shop/',
   pluginOptions: {
     quasar: {
       importStrategy: 'kebab',
